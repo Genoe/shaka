11218cb39a140eca311b9506453b7053c0e15585
Genoe <wweisensel@gmail.com>
2017-05-09 14:49:38 -0500

    Update secret.js

--- server/config/secret.js
+++ server/config/secret.js
@@ -1,4 +1,4 @@
 module.exports = function() {
   //return 'super.super.secret.shhh';
-  return 'what_would_be_considered_a_good_token_to_you_people???!!!8092345urifpaoifjajsklcn9q34208fhnawoeifnlv n49835fjlskfj;aslkfj;alknvalvn;alsns;dalkdvn439uroijfwealkjsd';
+  return 'xxxx_would_be_considered_a_good_token_to_you_people???!!!8092345urifpaoifjajsklcn9q34208fhnawoeifnlv n49835fjlskfj;aslkfj;alknvalvn;alsns;dalkdvn439uroijfwealkjsd';
 }
